e5181725b2046eb8a5a8964e91cee8da998af747
Rene Preissel <rene.preissel@etosquare.de>
2017-02-01 19:28:38 +0100

    cuberatura uns sonar

--- src/main/java/de/e2/helloworld/app/HelloWorldApplication.java
+++ src/main/java/de/e2/helloworld/app/HelloWorldApplication.java
@@ -2,15 +2,8 @@
 package de.e2.helloworld.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
-import org.springframework.boot.actuate.endpoint.MetricsEndpoint;
-import org.springframework.boot.actuate.endpoint.MetricsEndpointMetricReader;
-import org.springframework.boot.actuate.metrics.jmx.JmxMetricWriter;
-import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jmx.export.MBeanExporter;
 
 import de.e2.helloworld.lib.Greeter;
 
